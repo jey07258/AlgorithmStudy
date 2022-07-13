c5b287090be3493b776c1d869e03732e5bfdbbf6
jey07258 <108322804+jey07258@users.noreply.github.com>
2022-07-13 15:42:26 +0900

    baekjoon 11047
    그리디 1번

---
+++ Greedy/BJ11047.py
@@ -0,0 +1,14 @@
+N, K = map(int, input().split())
+result = 0
+coin_value = []
+
+for _ in range(N):
+    coin_value.append(int(input()))
+
+for coin in coin_value[::-1]:
+    if K==0: #해당 조건을 안붙여서 ZeroDivisionError를 냈음... 마지막까지 확인을 잘 하자
+        break
+    result += K // coin
+    K %= coin  
+
+print(result)
